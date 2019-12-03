6897be2be661d1c0f5b1746a4fe338e233c53be3
warsiss <57784887+warsiss@users.noreply.github.com>
2019-12-03 09:03:33 +0500

    Detection and tracking of objects in video

--- FirstVideoDetection.py
+++ FirstVideoDetection.py
@@ -8,7 +8,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
 detector.loadModel()
 
-video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join( execution_path, "traffic-mini.mp4"),
-                                output_file_path=os.path.join(execution_path, "traffic_mini_detected_1")
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join( execution_path, "vid.mp4"),
+                                output_file_path=os.path.join(execution_path, "detected_1")
                                 , frames_per_second=29, log_progress=True)
 print(video_path)
